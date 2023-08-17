585c6a7b79975df20d6f2563e80d2cb20ac4fa21
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-17 13:42:56 +0530

    Create register page and started server

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -45,7 +45,7 @@ export default function Header() {
       </div>
       <Link
         to="/login"
-        className='className="flex items-center gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-500"'
+        className="flex items-center gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-500"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div className="mt-4">
-      <h1 className="text-4xl text-center">Login</h1>
-      <form className="max-w-md mx-auto">
-        <input type="email" placeholder="your@email.com" />
-        <input type="password" placeholder="Your password" />
-        <button className="primary" type="submit">
-          Login
-        </button>
-      </form>
+    <div className="mt-4 grow flex items-center justify-around">
+      <div className="mb-48">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto">
+          <input type="email" placeholder="your@email.com" />
+          <input type="password" placeholder="Your password" />
+          <button className="primary" type="submit">
+            Login
+          </button>
+          <div className="text-center py-2 text-gray-500">
+            Don't have an account?{' '}
+            <Link className="underline text" to={'/register'}>
+              Register now
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
