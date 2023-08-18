79d7802357fffc8a6a75dc16204ce0fe7147ca51
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-18 22:39:32 +0530

    added axios and cors

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  function registerUser(ev) {
+    ev.preventDefault();
+    axios.get('/test');
+  }
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-48">
         <h1 className="text-4xl text-center mb-4">Register</h1>
-        <form className="max-w-md mx-auto">
-          <input type="text" placeholder="John Doe" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="Your password" />
+        <form className="max-w-md mx-auto" onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="John Doe"
+            value={name}
+            onChange={ev => setName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Your password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+          />
           <button className="primary" type="submit">
             Register
           </button>
