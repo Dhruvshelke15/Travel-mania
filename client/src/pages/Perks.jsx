0bfd7a3e01e0c26a1a3eeab1d84f6604f7b10b54
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-29 16:14:52 +0530

    added form for accomodation details

---
+++ client/src/pages/Perks.jsx
@@ -0,0 +1,118 @@
+export default function Perks({ selected, onChange }) {
+  return (
+    <>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8.25 18.75a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 01-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 00-3.213-9.193 2.056 2.056 0 00-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 00-10.026 0 1.106 1.106 0 00-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
+          />
+        </svg>
+
+        <span>Free Parking</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
+          />
+        </svg>
+
+        <span>Wifi</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" />
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+          />
+        </svg>
+
+        <span>Pets</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" />
+        <svg
+          class="h-8 w-8"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          fill="none"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          {' '}
+          <path stroke="none" d="M0 0h24v24H0z" />{' '}
+          <circle cx="11" cy="5" r="2" />{' '}
+          <polyline points="11 7 11 15 15 15 19 20" />{' '}
+          <line x1="11" y1="11" x2="16" y2="11" />{' '}
+          <path d="M7 11.5a4.97 4.97 0 1 0 6 7.5" />
+        </svg>
+
+        <span>Disability Access</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" />
+        <svg
+          class="h-8 w-8"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          {' '}
+          <path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2" />
+        </svg>
+        <span>A/C</span>
+      </label>
+      <label className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
+        <input type="checkbox" />
+        <svg
+          class="h-8 w-8 "
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          {' '}
+          <path d="M9.59 4.59A2 2 0 1 1 11 8H2m10.59 11.41A2 2 0 1 0 14 16H2m15.73-8.27A2.5 2.5 0 1 1 19.5 12H2" />
+        </svg>
+        <span>Gym</span>
+      </label>
+    </>
+  );
+}
--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import Perks from './Perks';
 
 export default function Places() {
   const { action } = useParams();
+  const [title, setTitle] = useState('');
+  const [address, setAddress] = useState('');
+  const [addedPhotos, setAddedPhotos] = useState([]);
+  const [description, setDescription] = useState('');
+  const [perks, setPerks] = useState([]);
+  const [extraInfo, setExtraInfo] = useState('');
+  const [checkIn, setCheckIn] = useState('');
+  const [checkOut, setCheckOut] = useState('');
+  const [maxGuests, setMaxGuests] = useState(1);
+
+  function inputHeader(text) {
+    return <h2 className="text-2xl mt-4">{text}</h2>;
+  }
+  function inputDescription(text) {
+    return <p className="text-gray-500 text-sm font-style: italic">{text}</p>;
+  }
+  function preInput(header, description) {
+    return (
+      <>
+        {inputHeader(header)}
+        {inputDescription(description)}
+      </>
+    );
+  }
+
   return (
     <div>
       {action !== 'new' && (
-        <div className="text-center">
+        <div className="text-center cursor-pointer">
           <Link
             className="inline-flex gap-1 bg-primary text-white py-2 px-6 rounded-full"
             to={'/account/places/new'}
@@ -31,28 +58,19 @@ export default function Places() {
       {action === 'new' && (
         <div>
           <form>
-            <h2 className="text-xl mt-4">Title</h2>
-            <p className="text-gray text-sm font-style: italic">
-              *Add a catchy title
-            </p>
+            {preInput('Title', '*Add a catchy title')}
             <input type="text" placeholder="title" />
-            <h2 className="text-xl mt-4">Address</h2>
-            <p className="text-gray text-sm font-style: italic">
-              *Address to this place
-            </p>
+
+            {preInput('Address', '*Address of your place')}
             <input type="text" placeholder="address" />
-            <h2 className="text-xl" mt-4>
-              Photos
-            </h2>
-            <p className="text-gray text-sm font-style: italic">
-              *The more the better!
-            </p>
-            <div className="flex gap-2">
+
+            {preInput('Photos', '*The more the better!')}
+            <div className="border p-4 flex rounded-2xl gap-2 items-center cursor-pointer">
               <input type="text" placeholder="Add using a link.." />
               <button className="bg-gray-200 grow px-4">Add&nbsp;photos</button>
             </div>
             <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-              <button className=" flex gap-1 justify-center border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+              <button className=" flex gap-1 justify-center cursor-pointer border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -70,19 +88,40 @@ export default function Places() {
                 Upload
               </button>
             </div>
-            <h2 className="text-xl" mt-4>
-              Description
-            </h2>
-            <p className="text-gray text-sm font-style: italic">
-              *Description of the place.
-            </p>
+            {preInput('Description', '*describe the place!')}
+
             <textarea></textarea>
-            <h2 className="text-xl" mt-4>
-              Perks
-            </h2>
-            <p className="text-gray text-sm font-style: italic">
-              *Description of the perks of the place.
-            </p>
+            {preInput('Perks', '*Select all the perks of this place')}
+            <div className="grid mt-2 grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-6">
+              <Perks selected={perks} onChange={setPerks} />
+            </div>
+            {preInput('Extra Info', '*Tell more about this place')}
+
+            <textarea />
+            {preInput(
+              'Check in & out times',
+              '*Mention time for check in & out'
+            )}
+
+            <div className="grid gap-2 sm: grid-cols-3">
+              <div>
+                <h3 className="mt-2 -mb-1">Check in time</h3>
+                <input type="text" placeholder="14:00" />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Check out time</h3>
+
+                <input type="text" placeholder="10:00" />
+              </div>
+              <div>
+                <h3 className="mt-2 -mb-1">Max number of guests</h3>
+
+                <input type="text" placeholder="4" />
+              </div>
+            </div>
+            <div>
+              <button className="primary my-4">Save</button>
+            </div>
           </form>
         </div>
       )}
