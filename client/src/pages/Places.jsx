d370e8613e4943b17fbab4383519708273678173
Dhruv Shelke <dhruvshelke588@gmail.com>
2023-08-31 16:20:21 +0530

    bug fixes

--- client/src/pages/Places.jsx
+++ client/src/pages/Places.jsx
@@ -101,14 +101,18 @@ export default function Places() {
                 Add&nbsp;photos
               </button>
             </div>
-            <div className="mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
+            <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
               {addedPhotos.length > 0 &&
                 addedPhotos.map(link => {
                   <div>
-                    <img src={'http://localhost:5173/uploads/' + link} />
+                    <img
+                      className="rounded-2xl"
+                      src={'http://localhost:4000/uploads/' + link}
+                      alt=""
+                    />
                   </div>;
                 })}
-              <button className=" flex gap-1 justify-center cursor-pointer border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
+              <button className=" flex items-center gap-1 justify-center cursor-pointer border bg-transparent rounded-2xl p-2 text-2xl text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
