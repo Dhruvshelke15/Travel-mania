dcc575487dfc56f5ee095f4418ebc6b4bf9009df
Dhruvshelke15 <dhruvshelke588@gmail.com>
2023-10-18 13:45:39 +0530

    bug fixes

--- client/src/Gallery.jsx
+++ client/src/Gallery.jsx
@@ -74,7 +74,6 @@ export default function Gallery({ place }) {
       </div>
       <button
         onClick={() => setShowAllPhotos(true)}
-        className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow-md shadow-gray-500"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
